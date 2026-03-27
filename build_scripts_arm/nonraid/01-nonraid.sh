784f06b51aceebcef531d91f0cfa7849c0f8e7fa
Christoph <christophhummer@gmail.com>
2026-03-27 16:17:14 +0100

    Add files via upload
    - multi arch compatibility

---
+++ build_scripts_arm/nonraid/01-nonraid.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Set variables
+DRIVER_NAME=nonraid
+DRIVER_BUILD_DIR=$BUILD_DIR/$DRIVER_NAME
+DRIVER_PACKAGE_DIR=$DRIVER_BUILD_DIR/package
+DRIVER_OUTPUT_DIR=$WORK_DIR/$KERNEL_V
+
+# Create driver build directory
+mkdir $DRIVER_BUILD_DIR
+cd $DRIVER_BUILD_DIR
+
+# Clone from Github, checkout master and get latest commit date
+git clone --depth 1 https://github.com/qvr/nonraid $DRIVER_NAME
+cd $DRIVER_BUILD_DIR/$DRIVER_NAME
+git checkout main
+DRIVER_V_PKG="$(git log -1 --format="%cs" | sed 's/-//g')"
+
+# Build driver
+cd $DRIVER_BUILD_DIR/$DRIVER_NAME
+
+# Fix for ARM64:
+cp $KERNEL_DIR/lib/raid6/recov_neon.c $DRIVER_BUILD_DIR/$DRIVER_NAME/raid6/recov_neon.c
+
+make -C $KERNEL_DIR M=$DRIVER_BUILD_DIR/$DRIVER_NAME modules CONFIG_UBSAN=n
+
+# Create directory, move modules to package directory and compress modules
+mkdir -p $DRIVER_PACKAGE_DIR/lib/modules/${KERNEL_V}-mos/kernel/drivers/md $DRIVER_PACKAGE_DIR/lib/modules/${KERNEL_V}-mos/kernel/drivers/scsi/raid6 $DRIVER_PACKAGE_DIR/usr/local/bin
+cp $DRIVER_BUILD_DIR/$DRIVER_NAME/md_nonraid/md-nonraid.ko $DRIVER_PACKAGE_DIR/lib/modules/${KERNEL_V}-mos/kernel/drivers/md/
+cp $DRIVER_BUILD_DIR/$DRIVER_NAME/raid6/nonraid6_pq.ko $DRIVER_PACKAGE_DIR/lib/modules/${KERNEL_V}-mos/kernel/drivers/scsi/raid6/
+while read -r module
+do
+  xz --check=crc32 --lzma2 $module
+done < <(find $DRIVER_PACKAGE_DIR/lib/modules/${KERNEL_V}-mos/kernel -name "*.ko")
+
+# Add nmdctl - partially supported in MOS API now
+#wget -q -O $DRIVER_PACKAGE_DIR/usr/local/bin/nmdctl https://raw.githubusercontent.com/qvr/nonraid/main/tools/nmdctl
+
+# Add License
+mkdir -p $DRIVER_PACKAGE_DIR/usr/share/doc/$DRIVER_NAME
+cat $DRIVER_BUILD_DIR/$DRIVER_NAME/LICENSE* >> $DRIVER_PACKAGE_DIR/usr/share/doc/$DRIVER_NAME/LICENSE
+
+# Create Debian control file
+mkdir $DRIVER_PACKAGE_DIR/DEBIAN
+cat > $DRIVER_PACKAGE_DIR/DEBIAN/control << EOF
+Package: ${DRIVER_NAME}-driver
+Version: $DRIVER_V_PKG
+Architecture: arm64
+Maintainer: ich777
+Description: $DRIVER_NAME-driver for MOS
+EOF
+
+# Create Debian package and md5 checksum
+cd $DRIVER_BUILD_DIR
+dpkg-deb --build package $DRIVER_OUTPUT_DIR/${DRIVER_NAME}_${DRIVER_V_PKG}-1+mos_arm64.deb
+
+# Check filesize
+MIN_SIZE=25000
+PACKAGE_SIZE=$(stat -c%s $DRIVER_OUTPUT_DIR/${DRIVER_NAME}_${DRIVER_V_PKG}-1+mos_arm64.deb)
+if [ "$PACKAGE_SIZE" -lt "$MIN_SIZE" ] ; then
+  echo "ERROR: Package filesize to low, deleting package: ${DRIVER_NAME}_${DRIVER_V_PKG}-1+mos_arm64.deb"
+  discord_push_notification "$DRIVER_NAME" "Compilation failed for $DRIVER_V_PKG" "1"
+  rm -f $DRIVER_OUTPUT_DIR/${DRIVER_NAME}_${DRIVER_V_PKG}-1+mos_arm64.deb
+else
+  md5sum $DRIVER_OUTPUT_DIR/${DRIVER_NAME}_${DRIVER_V_PKG}-1+mos_arm64.deb | awk '{print $1}' > $DRIVER_OUTPUT_DIR/${DRIVER_NAME}_${DRIVER_V_PKG}-1+mos_arm64.deb.md5
+fi
+exit 0
